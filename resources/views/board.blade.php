5ee1ef38b7ac4dac2b5a4e29a50487b3745316eb
Ecofreak572 <69659517+Ecofreak572@users.noreply.github.com>
2022-03-11 18:59:09 -0800

    Update board.blade.php

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -1,1 +1,19 @@
+@extends('adminlte::oage')
 
+@section('title', 'Board')
+
+@section('content_header')
+    <h1>Board</h1>
+@stop
+
+section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="cold-md-3 col-sm-6 col-12">
+            <div class="card card-row card-secondary">
+                <div class="card-header">
+                    <h3 class="card-title"> Backlog </h3>
+                    ::after
+                </div>
+                <div class="card-body">
+        
